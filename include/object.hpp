3d232dd9707047c50ec48b1f11312984a9a758d9
Shelvin Pauly <spauly@umd.edu>
2022-12-12 20:48:12 -0500

    [3.3.3]: Creating object.hpp

---
+++ include/object.hpp
@@ -0,0 +1,30 @@
+// Copyright Venkata Sairam Polina.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+
+#include <std_srvs/SetBool.h>
+#include <gazebo_msgs/ModelState.h>
+#include <gazebo_msgs/SpawnModel.h>
+
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <ros/ros.h>
+
+
+
