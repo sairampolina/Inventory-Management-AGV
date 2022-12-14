4ca2da1afa3af3aec560ec2704cb59e703efeb44
Shelvin Pauly <spauly@umd.edu>
2022-12-13 23:46:17 -0500

    [3.3.4]: Creating my_tiago.hpp

---
+++ include/my_tiago.hpp
@@ -0,0 +1,110 @@
+// Copyright Venkata Sairam Polina.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "manipulation.hpp"
+#include "navigation.hpp"
+#include "perception.hpp"
+#include "object.hpp"
+
+class MyTiago {
+    public:
+        /**
+        * @brief Construct a new DCRobot object
+        * 
+        */
+        explicit MyTiago(ros::NodeHandle*);
+
+        /**
+        * @brief Synchronizes various tasks of the robot
+        * 
+        */
+        void handle_states();
+
+        /**
+        * @brief Checks whether the object is within reach of the robot
+        * 
+        */
+        bool is_obj_within_reach();
+
+        /**
+        * @brief Gets object pose from the world. 
+        *    /**
+        * @brief Construct a new DCRobot object
+        * 
+        */
+    
+        geometry_msgs::Pose get_object_pose(std::string wrt = "map");
+
+        /**
+        * @brief Handles object picking by the robot.
+        * 
+        */
+        void pick_up_object();
+
+        /**
+        * @brief Handles object placement by the robot.
+        * 
+        */
+        void place_object();
+
+        /**
+        * @brief Enumerations various robot functioning states.
+        * 
+        */
+        enum robotState {
+            STARTING,
+            IDLE,
+            MOVING_TO_CHECKPOINT,
+            TURNING_AROUND,
+            OBJECT_FOUND,
+            MOVING_TOWARDS_OBJECT,
+            PICKING_OBJECT,
+            MOVING_TO_BIN_LOCATION,
+            PLACING_OBJECT,
+            STOP
+        };
+
+        /** Instance for Navigation class*/
+        Navigation navigator;
+        /** Instance for graspObj class*/
+        Manipulation manipulator;
+        /** Instance for DetectObject class*/
+        PackageDetector detector;
+
+    private:
+    /**
+        * @brief Enumerates various object states.
+        * 
+        */
+        void set_head_down();
+
+        /**
+        * @brief Enumerates various object states.
+        * 
+        */
+        void create_point_head_client(PointHeadClientPtr&);
+
+        /** Node Handle created*/
+        ros::NodeHandle* nh_;
+        /** TF buffer created*/
+        tf2_ros::Buffer tfbuffer_;
+        /** TF listener created*/
+        tf2_ros::TransformListener tflistener_;
+        /** PointHeadClientPtr for head movement actions*/
+        PointHeadClientPtr point_head_client_;
+        /** robotState for checking current state of the robot */
+        robotState state_;
+        /** robotState for storing previous states of the robot*/
+        robotState pre_state_;
+};
